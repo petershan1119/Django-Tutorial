56730e6c3e89c4c3bdfd02c70fd81611242b4176
Sang Won Han <peter.s.han.1119@gmail.com>
2018-02-02 15:28:27 +0900

    detail.html 추가

--- django/polls/urls.py
+++ django/polls/urls.py
@@ -1,7 +1,22 @@
-from django.urls import path
+from django.urls import path, re_path
 
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+
+    path('<int:question_id>/', views.detail, name='detail'),
+
+    path('<int:question_id>/results/', views.results, name='results'),
+
+    path('<int:question_id>/vote/', views.vote, name='vote'),
+
+    re_path(r'^$', views.index, name='index'),
+
+    re_path(r'^(?P<question_id>\d+)/$', views.detail, name='detail'),
+
+    re_path(r'^(?P<question_id>\d+)/results/$', views.results, name='results'),
+
+    re_path(r'^(?P<question_id>\d+)/vote/$', views.vote, name='vote'),
+
 ]
--- django/polls/views.py
+++ django/polls/views.py
@@ -1,6 +1,43 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+#from django.template import loader
 
+from .models import Question, Choice
 
 def index(request):
-    return HttpResponse("Hello, world. You're ate the polls index.")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    # output = ', '.join([q.question_text for q in latest_question_list])
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+
+    # template = loader.get_template('polls/index.html')
+    # return HttpResponse(template.render(context, request))
+
+    return render(request, 'polls/index.html', context)
+
+
+def detail(request, question_id):
+    """
+    question_id에 해당하는 Question객체를 탬플릿에 전달
+        key: "question"
+        template: polls/templates/polls/detail.html
+    템플릿에서는
+    1. 전달받은 question의 question_text를 출력
+    2. question이 가진 모든 Choice들을 ul > li로 출력
+    :param request:
+    :param question_id:
+    :return:
+    """
+    question = Question.objects.get(pk=question_id)
+    context = {
+        'question': question,
+    }
+    return render(request, 'polls/detail.html', context)
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
